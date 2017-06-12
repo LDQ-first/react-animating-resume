6727554c5d962aa7d2f4d704c44812eea1211547
LDQ-first <178848890@qq.com>
2017-06-12 22:33:05 +0800

    add style-component && build

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from '../static/img/logo.svg';
 import './App.css';
+import styled from 'styled-components'
+
+const AppDiv = styled.div`
+    text-align: center;
+`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <AppDiv>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
@@ -13,7 +18,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-      </div>
+      </AppDiv>
     );
   }
 }
